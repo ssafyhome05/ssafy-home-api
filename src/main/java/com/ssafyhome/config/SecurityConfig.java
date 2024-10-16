5a89e22fd79cac3bfe7ee48f5e78e900f79b3cb2
s12171934 <ssong99999@naver.com>
2024-10-16 11:59:51 +0900

    [REFACTOR] Extract getErrorDetail method into ErrorUtil class

--- src/main/java/com/ssafyhome/config/SecurityConfig.java
+++ src/main/java/com/ssafyhome/config/SecurityConfig.java
@@ -11,24 +11,21 @@ import com.ssafyhome.model.service.JWTService;
 import com.ssafyhome.model.service.impl.CustomOAuth2UserService;
 import com.ssafyhome.model.service.impl.CustomUserDetailsService;
 import com.ssafyhome.util.CookieUtil;
+import com.ssafyhome.util.ErrorUtil;
 import com.ssafyhome.util.JWTUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
@@ -36,13 +33,8 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-import java.lang.reflect.Method;
 import java.util.Collections;
-import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -58,8 +50,10 @@ public class SecurityConfig {
   private final JWTService jwtService;
   private final JWTUtil jwtUtil;
   private final CookieUtil cookieUtil;
+  private final ErrorUtil errorUtil;
   private final CustomUserDetailsService userDetailsService;
-  private final RequestMappingHandlerMapping handlerMapping;
+  private final CustomOAuth2UserService customOAuth2UserService;
+  private final CustomOAuth2SuccessHandler customOAuth2SuccessHandler;
 
   public SecurityConfig(
       AuthenticationConfiguration authenticationConfiguration,
@@ -68,8 +62,10 @@ public class SecurityConfig {
       JWTService jwtService,
       JWTUtil jwtUtil,
       CookieUtil cookieUtil,
+      ErrorUtil errorUtil,
       CustomUserDetailsService userDetailsService,
-      RequestMappingHandlerMapping handlerMapping
+      CustomOAuth2UserService customOAuth2UserService,
+      CustomOAuth2SuccessHandler customOAuth2SuccessHandler
   ) {
 
     this.authenticationConfiguration = authenticationConfiguration;
@@ -78,8 +74,10 @@ public class SecurityConfig {
     this.jwtService = jwtService;
     this.jwtUtil = jwtUtil;
     this.cookieUtil = cookieUtil;
+    this.errorUtil = errorUtil;
     this.userDetailsService = userDetailsService;
-    this.handlerMapping = handlerMapping;
+    this.customOAuth2UserService = customOAuth2UserService;
+    this.customOAuth2SuccessHandler = customOAuth2SuccessHandler;
   }
 
   @Bean
@@ -109,7 +107,7 @@ public class SecurityConfig {
       response.setContentType("application/json;charset=UTF-8");
 
       ObjectMapper mapper = new ObjectMapper();
-      String jsonResponse = mapper.writeValueAsString(getErrorDetails(request, accessDeniedException));
+      String jsonResponse = mapper.writeValueAsString(errorUtil.getErrorDetails(request, accessDeniedException));
       response.getWriter().write(jsonResponse);
     };
   }
@@ -122,48 +120,14 @@ public class SecurityConfig {
       response.setContentType("application/json;charset=UTF-8");
 
       ObjectMapper mapper = new ObjectMapper();
-      String jsonResponse = mapper.writeValueAsString(getErrorDetails(request, authException));
+      String jsonResponse = mapper.writeValueAsString(errorUtil.getErrorDetails(request, authException));
       response.getWriter().write(jsonResponse);
     };
   }
 
-  private Map<String, Object> getErrorDetails(HttpServletRequest request, Exception exception) {
-
-    String methodName;
-    String requiredAuthorities = "Unknown";
-
-    try {
-      HandlerMethod handlerMethod = (HandlerMethod) request.getAttribute(HandlerMapping.BEST_MATCHING_HANDLER_ATTRIBUTE);
-      Method method = handlerMethod.getMethod();
-      methodName = method.getDeclaringClass().getSimpleName() + "#" + method.getName();
-
-      PreAuthorize preAuthorize = method.getAnnotation(PreAuthorize.class);
-      if (preAuthorize != null) requiredAuthorities = preAuthorize.value();
-
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    String currentAuthorities = "No authorities";
-    if (authentication != null) {
-      currentAuthorities = authentication.getAuthorities().iterator().next().getAuthority();
-    }
-
-    return Map.of(
-        "error", exception instanceof AccessDeniedException ? "해당 접근에 대한 권한이 필요합니다." : "인증이 필요합니다.",
-        "message", exception.getMessage(),
-        "method", methodName,
-        "requiredAuthorities", requiredAuthorities,
-        "currentAuthorities", currentAuthorities
-    );
-  }
-
   @Bean
   public SecurityFilterChain securityFilterChain(
-      HttpSecurity http,
-      CustomOAuth2UserService customOAuth2UserService,
-      CustomOAuth2SuccessHandler customOAuth2SuccessHandler) throws Exception {
+      HttpSecurity http) throws Exception {
 
     http.csrf(AbstractHttpConfigurer::disable);
     http.formLogin(AbstractHttpConfigurer::disable);
