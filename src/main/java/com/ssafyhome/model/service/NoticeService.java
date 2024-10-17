4c914c1d33d02a6374c55599206107b16a5b17d6
s12171934 <ssong99999@naver.com>
2024-10-17 14:42:54 +0900

    [FEAT] set notice skeleton design

--- src/main/java/com/ssafyhome/model/service/NoticeService.java
+++ src/main/java/com/ssafyhome/model/service/NoticeService.java
@@ -1,4 +1,14 @@
 package com.ssafyhome.model.service;
 
+import com.ssafyhome.model.dto.NoticeDto;
+
+import java.util.List;
+
 public interface NoticeService {
+
+  void registerNotice(NoticeDto noticeDto);
+  NoticeDto getNotice(long noticeSeq);
+  List<NoticeDto> getNotices(int page);
+  void updateNotice(NoticeDto noticeDto);
+  void deleteNotice(long noticeSeq);
 }
--- src/main/java/com/ssafyhome/model/service/impl/NoticeServiceImpl.java
+++ src/main/java/com/ssafyhome/model/service/impl/NoticeServiceImpl.java
@@ -1,8 +1,36 @@
 package com.ssafyhome.model.service.impl;
 
+import com.ssafyhome.model.dto.NoticeDto;
 import com.ssafyhome.model.service.NoticeService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class NoticeServiceImpl implements NoticeService {
+
+  @Override
+  public void registerNotice(NoticeDto noticeDto) {
+
+  }
+
+  @Override
+  public NoticeDto getNotice(long noticeSeq) {
+    return null;
+  }
+
+  @Override
+  public List<NoticeDto> getNotices(int page) {
+    return List.of();
+  }
+
+  @Override
+  public void updateNotice(NoticeDto noticeDto) {
+
+  }
+
+  @Override
+  public void deleteNotice(long noticeSeq) {
+
+  }
 }
