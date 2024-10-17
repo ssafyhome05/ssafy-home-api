1dda0b969e59ec374c237b22e1052dd869a1fb9e
s12171934 <ssong99999@naver.com>
2024-10-17 14:36:32 +0900

    [FEAT] add uri prefix /api

--- src/main/java/com/ssafyhome/config/webConfig.java
+++ src/main/java/com/ssafyhome/config/webConfig.java
@@ -3,13 +3,19 @@ package com.ssafyhome.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsMvcConfig implements WebMvcConfigurer {
+public class webConfig implements WebMvcConfigurer {
 
   @Value("${front-end.url}")
   String frontEndUrl;
+
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configurer) {
+    configurer.addPathPrefix("/api", c -> true);
+  }
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
