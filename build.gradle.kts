af8a1c513b5880d75afc355a758e4939bdc44202
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-10-19 19:58:34 +0900

    Feature/openfeign (#6)
    * [BUILD] add openfeign dependency
    
    * [FEAT] enble opfeign in this project
    
    * [FEAT] change json parsing naming strategy
    
    * [FEAT] create feign client to get kakoKeywordPlace
    
    * [TEST] get keywordPlan with kakao API

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
 }
+val springCloudVersion by extra("2023.0.3")
 
 group = "com"
 version = "0.0.1-SNAPSHOT"
@@ -33,6 +34,7 @@ dependencies {
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
@@ -45,6 +47,11 @@ dependencies {
     testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
 
 tasks.withType<Test> {
---
+++ src/main/java/com/ssafyhome/model/dto/KakaoKeywordPlaceDto.java
@@ -0,0 +1,29 @@
+package com.ssafyhome.model.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class KakaoKeywordPlaceDto {
+
+	private List<Document> documents;
+
+	@Data
+	private static class Document {
+
+		private String placeName;
+		private String distance;
+		private String placeUrl;
+		private String categoryName;
+		private String addressName;
+		private String roadAddressName;
+		private String id;
+		private String phone;
+		private String categoryGroupCode;
+		private String categoryGroupName;
+		private String x;
+		private String y;
+	}
+
+}
