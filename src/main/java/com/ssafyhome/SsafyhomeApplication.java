321e6805e8b56a963e92cc51757e0fdb6e7b3a4f
s12171934 <ssong99999@naver.com>
2024-10-19 17:14:09 +0900

    [FEAT] enble opfeign in this project

--- src/main/java/com/ssafyhome/SsafyhomeApplication.java
+++ src/main/java/com/ssafyhome/SsafyhomeApplication.java
@@ -2,8 +2,10 @@ package com.ssafyhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SsafyhomeApplication {
 
 	public static void main(String[] args) {
