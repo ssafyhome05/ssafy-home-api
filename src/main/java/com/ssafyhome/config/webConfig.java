d3cbd717274b3a1158dd2ef4a919362738acf788
s12171934 <ssong99999@naver.com>
2024-10-20 20:29:17 +0900

    [FEAT] add interceptor : when user login, serve user profile info for view

--- src/main/java/com/ssafyhome/config/webConfig.java
+++ src/main/java/com/ssafyhome/config/webConfig.java
@@ -1,16 +1,25 @@
 package com.ssafyhome.config;
 
+import com.ssafyhome.middleware.interceptor.UserProfileInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class webConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
   @Value("${front-end.url}")
   String frontEndUrl;
+
+  private final UserProfileInterceptor userProfileInterceptor;
+
+  public WebConfig(UserProfileInterceptor userProfileInterceptor) {
+
+    this.userProfileInterceptor = userProfileInterceptor;
+  }
 
   @Override
   public void configurePathMatch(PathMatchConfigurer configurer) {
@@ -22,5 +31,12 @@ public class webConfig implements WebMvcConfigurer {
 
     registry.addMapping("/**")
         .allowedOrigins(frontEndUrl);
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry
+        .addInterceptor(userProfileInterceptor)
+        .addPathPatterns("/api/**");
   }
 }
