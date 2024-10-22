9b03879ebe96f9f6be681a165f0bfd769f4dd97e
s12171934 <ssong99999@naver.com>
2024-10-22 21:45:39 +0900

    [REFACTOR] [FEAT] change API package directory and seperate header config

--- src/main/java/com/ssafyhome/api/kakao/KakaoClient.java
+++ src/main/java/com/ssafyhome/api/kakao/KakaoClient.java
@@ -1,23 +1,23 @@
-package com.ssafyhome.api;
+package com.ssafyhome.api.kakao;
 
-import com.ssafyhome.config.FeignSnakeCaseConfig;
+import com.ssafyhome.api.common.FeignSnakeCaseConfig;
 import com.ssafyhome.model.dto.KakaoKeywordPlaceDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
 		name = "kakaoAPI",
 		url = "https://dapi.kakao.com",
-		configuration = FeignSnakeCaseConfig.class
+		configuration = {
+				FeignSnakeCaseConfig.class,
+				KakaoHeaderConfig.class
+		}
 )
 public interface KakaoClient {
 
 	@GetMapping("/v2/local/search/keyword.json")
 	KakaoKeywordPlaceDto searchKeywordPlace(
-			@RequestHeader("Authorization")
-			String apiKey,
 
 			@RequestParam("query")
 			String query
