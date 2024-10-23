5fed13a3246177478dde9a617812c1bc66e904ff
s12171934 <ssong99999@naver.com>
2024-10-24 06:00:45 +0900

    [RENAME] houseInfoSseEmitter -> houseInfoTask

--- src/main/java/com/ssafyhome/model/dto/house/HouseInfoTask.java
+++ src/main/java/com/ssafyhome/model/dto/house/HouseInfoTask.java
@@ -4,13 +4,13 @@ import lombok.Data;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Data
-public class HouseInfoSseEmitter {
+public class HouseInfoTask {
 
 	private SseEmitter emitter;
 	private int totalRows;
 	private String TaskName;
 
-	public HouseInfoSseEmitter(SseEmitter emitter) {
+	public HouseInfoTask(SseEmitter emitter) {
 
 		this.emitter = emitter;
 	}
