1887941780d9eb264d822c97d77d11e3e9e0841c
s12171934 <ssong99999@naver.com>
2024-10-23 23:07:32 +0900

    [FEAT] houseInfo and houseDeal data input

--- src/main/java/com/ssafyhome/controller/HouseController.java
+++ src/main/java/com/ssafyhome/controller/HouseController.java
@@ -5,11 +5,13 @@ import com.ssafyhome.model.dto.house.HouseDetailDto;
 import com.ssafyhome.model.dto.house.HouseDto;
 import com.ssafyhome.model.dto.house.HouseGraphDto;
 import com.ssafyhome.model.dto.spot.LocationStatusDto;
+import com.ssafyhome.model.service.HouseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 
@@ -21,6 +23,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/house")
 public class HouseController {
+
+	private final HouseService houseService;
+
+	public HouseController(HouseService houseService) {
+
+		this.houseService = houseService;
+	}
 
 	@Operation(
 			summary = "",
@@ -112,21 +121,17 @@ public class HouseController {
 			summary = "",
 			description = ""
 	)
-	@PostMapping("/deal")
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public ResponseEntity<?> updateHouseDealsInfo() {
+	@PostMapping("/admin/register")
+//	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public ResponseEntity<?> updateHouseInfo(
+			@RequestParam
+			int lawdCd,
 
-		return null;
-	}
+			@RequestParam
+			int dealYmd
+	) {
 
-	@Operation(
-			summary = "",
-			description = ""
-	)
-	@PostMapping("")
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public ResponseEntity<?> updateHouseInfo() {
-
+		houseService.insertHouseData(lawdCd, dealYmd);
 		return null;
 	}
 }
