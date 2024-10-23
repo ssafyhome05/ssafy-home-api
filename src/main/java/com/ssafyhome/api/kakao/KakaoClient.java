cda4847c3fa035dd8b054f5d8a1043bcfd39d67f
s12171934 <ssong99999@naver.com>
2024-10-23 11:16:44 +0900

    [FEAT] kakao category search api connection

--- src/main/java/com/ssafyhome/api/kakao/KakaoClient.java
+++ src/main/java/com/ssafyhome/api/kakao/KakaoClient.java
@@ -1,7 +1,7 @@
 package com.ssafyhome.api.kakao;
 
 import com.ssafyhome.api.common.FeignSnakeCaseConfig;
-import com.ssafyhome.model.dto.KakaoKeywordPlaceDto;
+import com.ssafyhome.model.dto.KakaoPlaceDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,9 +17,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface KakaoClient {
 
 	@GetMapping("/v2/local/search/keyword.json")
-	KakaoKeywordPlaceDto searchKeywordPlace(
+	KakaoPlaceDto searchKeywordPlace(
 
 			@RequestParam("query")
 			String query
+	);
+
+	@GetMapping("/v2/local/search/category.json")
+	KakaoPlaceDto searchCategoryPlace(
+
+			@RequestParam("category_group_code")
+			String code
 	);
 }
