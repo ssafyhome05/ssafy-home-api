408724f0d0606a6e5aabfa402f2ab9624970f201
s12171934 <ssong99999@naver.com>
2024-10-23 23:07:58 +0900

    [FEAT] house mapper and sql

--- src/main/java/com/ssafyhome/model/dao/mapper/HouseMapper.java
+++ src/main/java/com/ssafyhome/model/dao/mapper/HouseMapper.java
@@ -1,7 +1,14 @@
 package com.ssafyhome.model.dao.mapper;
 
+import com.ssafyhome.model.entity.mysql.DongCodeEntity;
+import com.ssafyhome.model.entity.mysql.HouseDealEntity;
+import com.ssafyhome.model.entity.mysql.HouseInfoEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface HouseMapper {
+
+	void insertHouseInfo(HouseInfoEntity houseInfoEntity);
+	void insertHouseDeal(HouseDealEntity houseDealEntity);
+	DongCodeEntity getSidoGugun(String dongCode);
 }
