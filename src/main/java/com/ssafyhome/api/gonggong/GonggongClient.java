928b5c836f4bca53a300e1a49b6804f3c737dc08
s12171934 <ssong99999@naver.com>
2024-10-23 20:42:49 +0900

    [FEAT] get AptTradeData from 공공데이터포탈

---
+++ src/main/java/com/ssafyhome/api/gonggong/GonggongClient.java
@@ -0,0 +1,34 @@
+package com.ssafyhome.api.gonggong;
+
+import com.ssafyhome.api.common.FeignXmlDecoderConfig;
+import com.ssafyhome.model.dto.api.GonggongAptTradeResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+		name = "gonggongAPI",
+		url = "https://apis.data.go.kr",
+		configuration = FeignXmlDecoderConfig.class
+)
+public interface GonggongClient {
+
+	@GetMapping("/1613000/RTMSDataSvcAptTradeDev/getRTMSDataSvcAptTradeDev")
+	GonggongAptTradeResponse getRTMSDataSvcAptTradeDev(
+
+			@RequestParam("LAWD_CD")
+			int lawdCd,
+
+			@RequestParam("DEAL_YMD")
+			int dealYmd,
+
+			@RequestParam("serviceKey")
+			String serviceKey,
+
+			@RequestParam("pageNo")
+			int pageNo,
+
+			@RequestParam("numOfRows")
+			int numsOfRows
+	);
+}
