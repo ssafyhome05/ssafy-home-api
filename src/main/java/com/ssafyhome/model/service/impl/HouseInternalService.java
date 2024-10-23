34340004e9bc16d049ef37cf463ad7c8cd47e1e5
s12171934 <ssong99999@naver.com>
2024-10-24 06:02:59 +0900

    [REFACTOR] seperate transactional inner method

---
+++ src/main/java/com/ssafyhome/model/service/impl/HouseInternalService.java
@@ -0,0 +1,122 @@
+package com.ssafyhome.model.service.impl;
+
+import com.ssafyhome.api.gonggong.GonggongClient;
+import com.ssafyhome.api.sgis.SGISClient;
+import com.ssafyhome.api.sgis.SGISUtil;
+import com.ssafyhome.model.dao.mapper.HouseMapper;
+import com.ssafyhome.model.dto.api.GonggongAptTradeResponse;
+import com.ssafyhome.model.dto.api.SgisGeoCode;
+import com.ssafyhome.model.dto.house.HouseInfoTask;
+import com.ssafyhome.model.entity.mysql.DongCodeEntity;
+import com.ssafyhome.model.entity.mysql.HouseDealEntity;
+import com.ssafyhome.model.entity.mysql.HouseInfoEntity;
+import com.ssafyhome.util.ConvertUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Service
+public class HouseInternalService {
+
+	@Value("${gonggong.API-KEY.decode}")
+	private String gonggongApiKey;
+
+	private final GonggongClient gonggongClient;
+	private final HouseMapper houseMapper;
+	private final ConvertUtil convertUtil;
+	private final SGISClient sgisClient;
+	private final SGISUtil sgisUtil;
+
+	public HouseInternalService(
+			GonggongClient gonggongClient,
+			HouseMapper houseMapper,
+			ConvertUtil convertUtil,
+			SGISClient sgisClient,
+			SGISUtil sgisUtil
+	) {
+
+		this.gonggongClient = gonggongClient;
+		this.houseMapper = houseMapper;
+		this.convertUtil = convertUtil;
+		this.sgisClient = sgisClient;
+		this.sgisUtil = sgisUtil;
+	}
+
+	@Transactional
+	protected Duration insertHouseData(int lawdCd, int dealYmd, HouseInfoTask houseInfoTask) {
+
+		LocalDateTime start = LocalDateTime.now();
+		int totalRows = 0 , repeat = 1, seq = 1;
+		do {
+
+			GonggongAptTradeResponse response =
+					gonggongClient.getRTMSDataSvcAptTradeDev(lawdCd, dealYmd, gonggongApiKey, repeat, 100);
+			if (totalRows == 0) {
+				totalRows = response.getBody().getTotalCount();
+				houseInfoTask.setTaskName(lawdCd + "-" + dealYmd);
+				houseInfoTask.setTotalRows(totalRows);
+				try {
+					houseInfoTask.getEmitter().send(
+							SseEmitter.event()
+									.name(houseInfoTask.getTaskName())
+									.data("Total rows: " + houseInfoTask.getTotalRows())
+					);
+				} catch (Exception e) {}
+			}
+
+			for(GonggongAptTradeResponse.Item item : response.getBody().getItems()) {
+
+				HouseInfoEntity houseInfoEntity = convertUtil.convert(item, HouseInfoEntity.class);
+				houseInfoEntity.setHouseSeq(item.getAptSeq());
+
+				DongCodeEntity dongCodeEntity = houseMapper.getSidoGugun(item.getSggCd() + item.getUmdCd());
+				String dongName;
+				try {
+					dongName = dongCodeEntity.getSidoName() + " " + dongCodeEntity.getGugunName() + " " + dongCodeEntity.getDongName();
+				} catch (Exception e) {
+					continue;
+				}
+
+
+				if(!houseMapper.isExistHouseInfo(houseInfoEntity.getHouseSeq())) {
+					SgisGeoCode geoCode = sgisClient.getGeocode(sgisUtil.getAccessToken(), dongName + " " + item.getJibun());
+					if(geoCode.getResult() != null){
+						SgisGeoCode.Result.ResultData resultData =
+								sgisClient.getGeocode(sgisUtil.getAccessToken(), dongName + " " + item.getJibun())
+										.getResult().getResultdata().get(0);
+						houseInfoEntity.setLatitude(resultData.getY());
+						houseInfoEntity.setLongitude(resultData.getX());
+					}
+					else {
+						try {
+							houseInfoTask.getEmitter().send(
+									SseEmitter.event()
+											.name("Not found jibun")
+											.data(lawdCd + "-" + dealYmd + "-" + seq)
+							);
+						} catch (Exception e) {}
+					}
+
+					try{
+						houseMapper.insertHouseInfo(houseInfoEntity);
+					} catch (DuplicateKeyException e) {}
+				}
+
+				HouseDealEntity houseDealEntity = convertUtil.convert(item, HouseDealEntity.class);
+				houseDealEntity.setDealSeq(lawdCd + "-" + dealYmd + "-" + seq++);
+
+				try{
+					houseMapper.insertHouseDeal(houseDealEntity);
+				} catch (DuplicateKeyException e) {}
+			}
+
+		} while (repeat++ * 100 < totalRows);
+		LocalDateTime end = LocalDateTime.now();
+		return Duration.between(start, end);
+	}
+}
--- src/main/java/com/ssafyhome/model/service/impl/HouseServiceImpl.java
+++ src/main/java/com/ssafyhome/model/service/impl/HouseServiceImpl.java
@@ -1,24 +1,14 @@
 package com.ssafyhome.model.service.impl;
 
-import com.ssafyhome.api.gonggong.GonggongClient;
-import com.ssafyhome.api.sgis.SGISClient;
-import com.ssafyhome.api.sgis.SGISUtil;
 import com.ssafyhome.model.dao.mapper.HouseMapper;
-import com.ssafyhome.model.dto.api.GonggongAptTradeResponse;
-import com.ssafyhome.model.dto.api.SgisGeoCode;
-import com.ssafyhome.model.dto.house.HouseInfoSseEmitter;
-import com.ssafyhome.model.entity.mysql.DongCodeEntity;
-import com.ssafyhome.model.entity.mysql.HouseDealEntity;
-import com.ssafyhome.model.entity.mysql.HouseInfoEntity;
+import com.ssafyhome.model.dto.house.HouseInfoTask;
 import com.ssafyhome.model.service.HouseService;
-import com.ssafyhome.util.ConvertUtil;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -29,52 +19,60 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class HouseServiceImpl implements HouseService {
 
-	private final Map<String, HouseInfoSseEmitter> sseEmitters = new ConcurrentHashMap<>();
+	private final Map<String, HouseInfoTask> sseEmitters = new ConcurrentHashMap<>();
 
-	@Value("${gonggong.API-KEY.decode}")
-	private String gonggongApiKey;
-
-	private final GonggongClient gonggongClient;
 	private final HouseMapper houseMapper;
-	private final ConvertUtil convertUtil;
-	private final SGISClient sgisClient;
-	private final SGISUtil sgisUtil;
+	private final HouseInternalService houseInternalService;
 
 	public HouseServiceImpl(
-			GonggongClient gonggongClient,
 			HouseMapper houseMapper,
-			ConvertUtil convertUtil,
-			SGISClient sgisClient,
-			SGISUtil sgisUtil
+			HouseInternalService houseInternalService
 	) {
 
-		this.gonggongClient = gonggongClient;
 		this.houseMapper = houseMapper;
-		this.convertUtil = convertUtil;
-		this.sgisClient = sgisClient;
-		this.sgisUtil = sgisUtil;
+		this.houseInternalService = houseInternalService;
 	}
 
 	@Override
-	public String startHouseInfoTask(int dealYmd) {
+	public String startHouseInfoTask(int dealYmd, int startLawdCd) {
 
 		String requestId = UUID.randomUUID().toString();
 
 
 		SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
-		sseEmitters.put(requestId, new HouseInfoSseEmitter(sseEmitter));
+		sseEmitters.put(requestId, new HouseInfoTask(sseEmitter));
 
 		CompletableFuture.runAsync(() -> {
 			try {
+				LocalDateTime start = LocalDateTime.now();
+				sseEmitter.send(SseEmitter.event()
+						.name("Task start time")
+						.data(start.format(timeFormatter()))
+				);
 				List<Integer> lawdCdList = houseMapper.getLawdCdList();
 				int seq = 1;
+				int size = lawdCdList.size();
 				for (int lawdCd : lawdCdList) {
-					insertHouseData(lawdCd, dealYmd, requestId);
+					if (lawdCd < startLawdCd) {
+						size--;
+						continue;
+					}
+					Duration partTime = houseInternalService.insertHouseData(lawdCd, dealYmd, sseEmitters.get(requestId));
 					sseEmitter.send(SseEmitter.event()
 							.name(sseEmitters.get(requestId).getTaskName())
-							.data("Task completed!! commit (" + seq++ + "/" + lawdCdList.size() + ")" )
+							.data("Task completed!! commit (" + seq++ + "/" + size + ") " + partTime.toSeconds() + " seconds")
 					);
 				}
+				LocalDateTime end = LocalDateTime.now();
+				sseEmitter.send(SseEmitter.event()
+						.name("Task end time")
+						.data(end.format(timeFormatter()))
+				);
+				Duration duration = Duration.between(start, end);
+				sseEmitter.send(SseEmitter.event()
+						.name("Task duration")
+						.data(String.format("%d:%d:%d", duration.toHours(), duration.toMinutes(), duration.toSeconds()))
+				);
 			} catch (Exception e) {
 				sseEmitter.completeWithError(e);
 			} finally {
@@ -94,64 +92,10 @@ public class HouseServiceImpl implements HouseService {
 		return sseEmitters.get(requestId).getEmitter();
 	}
 
-	@Transactional
-	protected void insertHouseData(int lawdCd, int dealYmd, String requestId) {
+	private DateTimeFormatter timeFormatter() {
 
-		int totalRows = 0 , repeat = 1, seq = 1;
-		do {
-
-			GonggongAptTradeResponse response =
-					gonggongClient.getRTMSDataSvcAptTradeDev(lawdCd, dealYmd, gonggongApiKey, repeat, 100);
-			if (totalRows == 0) {
-				totalRows = response.getBody().getTotalCount();
-				sseEmitters.get(requestId).setTaskName(lawdCd + "-" + dealYmd);
-				sseEmitters.get(requestId).setTotalRows(totalRows);
-				try {
-					HouseInfoSseEmitter houseInfoTask = sseEmitters.get(requestId);
-					houseInfoTask.getEmitter().send(
-							SseEmitter.event()
-									.name(houseInfoTask.getTaskName())
-									.data("Total rows: " + houseInfoTask.getTotalRows())
-					);
-				} catch (Exception e) {}
-			}
-
-			for(GonggongAptTradeResponse.Item item : response.getBody().getItems()) {
-
-				HouseInfoEntity houseInfoEntity = convertUtil.convert(item, HouseInfoEntity.class);
-				houseInfoEntity.setHouseSeq(item.getAptSeq());
-
-				DongCodeEntity dongCodeEntity = houseMapper.getSidoGugun(item.getSggCd() + item.getUmdCd());
-				String dongName = dongCodeEntity.getSidoName() + " " + dongCodeEntity.getGugunName() + " " + dongCodeEntity.getDongName();
-
-				SgisGeoCode.Result.ResultData geoCode =
-						sgisClient.getGeocode(sgisUtil.getAccessToken(), dongName + " " + item.getJibun())
-								.getResult().getResultdata().get(0);
-				houseInfoEntity.setLatitude(geoCode.getY());
-				houseInfoEntity.setLongitude(geoCode.getX());
-
-				HouseDealEntity houseDealEntity = convertUtil.convert(item, HouseDealEntity.class);
-				houseDealEntity.setDealSeq(lawdCd + "-" + dealYmd + "-" + seq++);
-
-				String alert = "";
-				try{
-					houseMapper.insertHouseInfo(houseInfoEntity);
-					alert += houseInfoEntity.getHouseSeq() + ": commit ready\n";
-				} catch (DuplicateKeyException e) {
-					alert += houseInfoEntity.getHouseSeq() + " is already exist\n";
-				}
-				try{
-					houseMapper.insertHouseDeal(houseDealEntity);
-					alert += houseDealEntity.getDealSeq() + ": commit ready";
-				} catch (DuplicateKeyException e) {
-					alert += houseDealEntity.getDealSeq() + " is already exist";
-				}
-
-				try {
-					sseEmitters.get(requestId).getEmitter().send(SseEmitter.event().name(alert));
-				} catch (Exception e) {}
-			}
-
-		} while (repeat++ * 100 < totalRows);
+		return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	}
 }
+
+
