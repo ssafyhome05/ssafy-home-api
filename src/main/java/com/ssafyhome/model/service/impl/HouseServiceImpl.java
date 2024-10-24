ae141641cad04a3d22f37793e5ce755e7128f752
박해구 <qkrgorn3617@naver.com>
2024-10-24 20:21:00 +0900

    [FIX] conflict solve

--- src/main/java/com/ssafyhome/model/service/impl/HouseServiceImpl.java
+++ src/main/java/com/ssafyhome/model/service/impl/HouseServiceImpl.java
@@ -59,23 +59,6 @@ public class HouseServiceImpl implements HouseService {
         return houseDealsList;
     }
 
-    @Override
-    public List<HouseDto> getHouseInfo(String dongCode) {
-
-        List<HouseDto> houseInfoList = houseMapper.getHouseInfo(dongCode);
-
-        return houseInfoList;
-    }
-
-    @Override
-    public List<HouseDealsDto> getHouseDeals(String houseSeq, int page, int limit) {
-
-        int offset = page * limit;
-        List<HouseDealsDto> houseDealsList = houseMapper.getHouseDeals(houseSeq, limit, offset);
-
-        return houseDealsList;
-    }
-
 	@Override
 	public String startHouseInfoTask(int dealYmd, int startCd, int endCd) {
 
