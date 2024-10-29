112578a41f8f27a18fbe9e002e0a9a33bcb3a35f
s12171934 <ssong99999@naver.com>
2024-10-29 10:28:11 +0900

    [TEST] 1111010100 법정동에 대한 최소경계원 테스트

---
+++ src/test/java/com/ssafyhome/GeometryTest.java
@@ -0,0 +1,31 @@
+package com.ssafyhome;
+
+import com.ssafyhome.model.dao.mapper.GeometryMapper;
+import com.ssafyhome.util.GeometryUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GeometryTest {
+
+    private final GeometryUtil geometryUtil;
+    private final GeometryMapper geometryMapper;
+
+    @Autowired
+    public GeometryTest(
+            GeometryUtil geometryUtil,
+            GeometryMapper geometryMapper
+    ) {
+
+        this.geometryUtil = geometryUtil;
+        this.geometryMapper = geometryMapper;
+    }
+
+    @Test
+    public void updateCircleTest() {
+
+        geometryUtil.updateCenterCircle("1111010100");
+        System.out.println(geometryUtil.getPoints(geometryMapper.selectByDongCode("1111010100")));
+    }
+}
