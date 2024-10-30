d11d9c5966066d71d097c875a0c512b960a6f727
s12171934 <ssong99999@naver.com>
2024-10-30 09:10:52 +0900

    [FEAT] config redisson client

--- src/main/java/com/ssafyhome/config/RedisConfig.java
+++ src/main/java/com/ssafyhome/config/RedisConfig.java
@@ -1,10 +1,14 @@
 package com.ssafyhome.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
@@ -19,5 +23,21 @@ public class RedisConfig {
   public RedisConnectionFactory redisConnectionFactory() {
 
     return new LettuceConnectionFactory(host, port);
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(redisConnectionFactory());
+    return template;
+  }
+
+  @Bean
+  public RedissonClient redissonClient() {
+
+    Config config = new Config();
+    config.useSingleServer().setAddress("redis://" + host + ":" + port);
+    return Redisson.create(config);
   }
 }
