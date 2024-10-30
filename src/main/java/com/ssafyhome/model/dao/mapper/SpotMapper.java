d1e56c6453d8d06db7e437b82e21e9d4ea404d92
s12171934 <ssong99999@naver.com>
2024-10-30 09:15:10 +0900

    [FEAT] create spot mapper to get kakao place

--- src/main/java/com/ssafyhome/model/dao/mapper/SpotMapper.java
+++ src/main/java/com/ssafyhome/model/dao/mapper/SpotMapper.java
@@ -1,7 +1,16 @@
 package com.ssafyhome.model.dao.mapper;
 
+import com.ssafyhome.model.entity.mysql.CategoryEntity;
+import com.ssafyhome.model.entity.mysql.SpotEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface SpotMapper {
+
+    List<CategoryEntity> getCategories();
+    List<SpotEntity> getSpotsByCategory(@Param("dongCode") String dongCode, @Param("category") String category);
+    void insertSpots(List<SpotEntity> spots);
 }
