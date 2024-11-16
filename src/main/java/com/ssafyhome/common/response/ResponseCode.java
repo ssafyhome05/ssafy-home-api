9bae9e14f8019f130748b89c7f51d4a89c80b08f
s12171934 <ssong99999@naver.com>
2024-11-16 17:23:08 +0900

    [FEAT] ResponseCode를 interface화 도메인별 코드 분리

--- src/main/java/com/ssafyhome/common/response/ResponseCode.java
+++ src/main/java/com/ssafyhome/common/response/ResponseCode.java
@@ -1,22 +1,10 @@
 package com.ssafyhome.common.response;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
-public enum ResponseCode {
+public interface ResponseCode {
 
-	OK(20000, "OK", HttpStatus.OK),
-	FAIL_SEND_MAIL(50000, "메일 전송에 실패했습니다", HttpStatus.INTERNAL_SERVER_ERROR);
-
-	private final int code;
-	private final String massage;
-	private final HttpStatus httpStatus;
-
-	ResponseCode(int code, String massage, HttpStatus httpStatus) {
-
-		this.code = code;
-		this.massage = massage;
-		this.httpStatus = httpStatus;
-	}
+	int getCode();
+	String getMessage();
+	HttpStatus getHttpStatus();
 }
