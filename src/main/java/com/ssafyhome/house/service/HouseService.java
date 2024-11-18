02eec54741fa1e96a0289e856fbf47f09accebe8
s12171934 <ssong99999@naver.com>
2024-11-18 22:41:55 +0900

    [HOTFIX]HouseService import문

--- src/main/java/com/ssafyhome/house/service/HouseService.java
+++ src/main/java/com/ssafyhome/house/service/HouseService.java
@@ -1,14 +1,9 @@
 package com.ssafyhome.house.service;
 
 import java.util.List;
-import java.util.Map;
 
-import com.ssafyhome.house.dto.HouseGraphDto;
-import com.ssafyhome.house.dto.HouseSearchWithTimeDto;
+import com.ssafyhome.house.dto.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import com.ssafyhome.house.dto.HouseDealDto;
-import com.ssafyhome.house.dto.HouseDto;
 
 public interface HouseService {
     List<HouseDto> getHouseInfo(HouseSearchWithTimeDto searchDto);
