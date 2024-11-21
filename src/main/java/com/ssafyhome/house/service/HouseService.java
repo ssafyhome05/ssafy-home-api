9c6cee6fe0f0dd68059e038c6023f928b315efb0
oha <x22z@naver.com>
2024-11-21 17:08:02 +0900

    [FEAT] 저장된 news 테이블에서 data get

--- src/main/java/com/ssafyhome/house/service/HouseService.java
+++ src/main/java/com/ssafyhome/house/service/HouseService.java
@@ -3,9 +3,6 @@ package com.ssafyhome.house.service;
 import java.util.List;
 
 import com.ssafyhome.common.util.object.Point;
-import com.ssafyhome.house.dto.HouseGraphDto;
-
-import com.ssafyhome.common.api.news.dto.NewsDto;
 import com.ssafyhome.house.dto.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -24,6 +21,6 @@ public interface HouseService {
     List<HouseDto> getHouseInfo(HouseSearchWithTimeDto searchDto);
     List<HouseDealDto> getHouseDealList(String houseSeq, int page, int limit);
     List<HouseGraphDto> getHouseGraph(String houseSeq, int year);
-    NewsDto getNews();
+    List<NewsDto> getNewsList();
     List<Point> getPoints(String dongCode);
 }
